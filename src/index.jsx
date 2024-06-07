9c01eb11ed558ad5dac05a5fa90172133c7ff5c9
programrsky <lovesky00317@gmail.com>
2024-06-07 15:22:55 +0900

    삭제 커밋

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import router from './utils/Router';
-import { RouterProvider } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={router} />);
